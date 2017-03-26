44a83494234eefc28a296ff39934cff489a932e8
Martin Rojo <wat.martinrojo@gmail.com>
2017-03-26 04:06:07 +0100

    .

--- template/src/router/index.js
+++ template/src/router/index.js
@@ -1,15 +1,15 @@
-import Vue from 'vue'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
-import Router from 'vue-router'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
-import Hello from '@/components/Hello'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+import Vue from 'vue'
+import Router from 'vue-router'
+import notFoundComponent from '@/components/notFoundComponent'
+import hello from '@/components/hello'
 
-Vue.use(Router){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
+  base: __dirname,
   routes: [
-    {
-      path: '/',
-      name: 'Hello',
-      component: Hello{{#if_eq lintConfig "airbnb"}},{{/if_eq}}
-    }{{#if_eq lintConfig "airbnb"}},{{/if_eq}}
-  ]{{#if_eq lintConfig "airbnb"}},{{/if_eq}}
-}){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+    { path: '/', name: 'Hello', component: hello },
+    { path: '*', component: notFoundComponent }
+  ]
+})
